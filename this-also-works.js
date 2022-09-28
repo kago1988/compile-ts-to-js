ec1c900ce6521c576f20fc4b42005015834130ef
Andreas Kagoshima <35202177+kago1988@users.noreply.github.com>
2022-09-28 22:37:42 +0200

    Update this-also-works.js

--- this-also-works.js
+++ this-also-works.js
@@ -6,6 +6,5 @@ function Person(name) {
 const person = new Person("Andreas");
 console.log(person)
 // Output: Person { name: 'Andreas' }
-// A discussion, why this also works but this is still the "wrong" compilation can be 
-// found on stackoverflow: 
+// A discussion, why this also works but this is still not recommended can be found on stackoverflow: 
 // https://stackoverflow.com/questions/61468113/typescript-is-it-possible-to-declare-a-class-using-function
